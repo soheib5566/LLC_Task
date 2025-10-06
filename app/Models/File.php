3dc08bd6cde46b50981c44942de06b2c3f03a979
soheib5566 <soheib5566@gmail.com>
2025-10-06 10:51:37 +0300

    add relathionships between User, Application, and File models

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    public function application()
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
